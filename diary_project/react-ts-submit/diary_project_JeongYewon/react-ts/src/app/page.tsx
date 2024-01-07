49ba4f525b925013061c4a3858d3318c0df3071d
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2024-01-08 02:36:35 +0900

    main page 재수정

--- diary_project/react-ts-submit/diary_project_JeongYewon/react-ts/src/app/page.tsx
+++ diary_project/react-ts-submit/diary_project_JeongYewon/react-ts/src/app/page.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Diary } from '../interface/diary'
 
-export default function DiaryHomePage() {
+const DiaryWriter = () => {
+    const [title, setTitle]=useState('');
+    const [contents, setContents]=useState('');
     const feelings = ['bad', 'soso','good','great','awesome'];
     const [selectedFeeling, setSelectedFeeling] = useState('');
     const weathers = ['cloud', 'rain','snow','sunny'];
     const [selectedWeather, setSelectedWeather] = useState('');
+    const [isValid, setValid] = useState(false);
 
-    const handleFeelingClick = (feeling:string) => { // 타입 지정 안하면 에러 뜨는데 출력은 됨.. 되는건가?
+    const handleFeelingClick = (feeling:string) => {
         setSelectedFeeling(feeling);
     }
 
@@ -14,48 +18,81 @@ export default function DiaryHomePage() {
         setSelectedWeather(weather);
     }
 
+    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value)
+    }
+    const handleContentsChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setContents(e.target.value)
+    }
+
+    useEffect(() => {
+        const invalidDiary =
+            selectedFeeling === '' || selectedWeather === '' || title.length <= 2 || contents.length <= 5
+
+        setValid(!invalidDiary)
+    }, [title, contents, feelings, weathers])
+
+    return (
+        <form id="write-form" className="flex-col gap-4 p-4 rounded-lg bg-white border border-gray-100 h-2/3">
+        <input
+            required
+            type="text" 
+            //name="title" 
+            placeholder="제목을 적어보세요" 
+            className="text-2xl p-2 mt-4 rounded-lg border border-black-100 w-full"
+            onChange={handleTitleChange}/>
+        
+        <div className="flex mt-6 gap-2">
+            {feelings.map((feeling,index)=>(
+                <button
+                key={index}
+                type="button"
+                className={`p-1 rounded-lg text-sm ${selectedFeeling === feeling ? 'bg-emerald-100 text-emerald-600' : 'bg-gray-100 text-gray-400'}`}
+                name="feeling"
+                value={feeling}
+                onClick={()=>handleFeelingClick(feeling)}>
+                    {feeling}
+                </button>
+            ))}
+        </div>
+        {/* 날씨 버튼 묶기 */}
+        <div className="flex mt-2 mb-6 gap-2">
+            {weathers.map((weather,index)=>(
+                <button 
+                key={index} 
+                type="button" 
+                className={`p-1 rounded-lg text-sm ${selectedWeather === weather ? 'bg-blue-100 text-blue-600' : 'bg-gray-100 text-gray-400'}`}
+                name="weather" 
+                value={weather}
+                onClick={()=>handleWeatherClick(weather)}>
+                    {weather}
+                </button>
+            ))}
+        </div>
+        
+        <textarea 
+            required 
+            //name="content" 
+            placeholder="오늘 당신의 하루는 어땠나요?" 
+            className="p-1 rounded-lg resize-none border border-black-100 h-1/2 w-full"
+            onChange={handleContentsChange}/>
+        <div>
+            <button 
+                type="submit"
+                className={`p-2 rounded-lg w-full ${isValid ? 'bg-emerald-100 text-emerald-600' : 'bg-gray-100 text-gray-400'}`}
+            >
+                {isValid ? '일기를 저장해 보아요' : '일기를 더 자세히 적어볼까요?'}
+            </button>
+        </div>
+    </form>     
+    )
+}
+
+export default function DiaryHomePage() {
     return (
         <div className="flex flex-col items-center justify-center gap-10 h-full md:grid md:grid-rows-1 md:grid-cols-[3fr,2fr] md:w-4/5 lg:w-2/3">
             {/* 첫화면 왼쪽 박스 */}
-            <form id="write-form" className="flex-col gap-4 p-4 rounded-lg bg-white border border-gray-100 h-2/3">
-                <input required type="text" name="title" placeholder="제목을 적어보세요" className="text-2xl p-2 mt-4 rounded-lg border border-black-100 w-full"/>
-                
-                {/* 기분 버튼 묶기 
-                    왜 "flex"없이 "gap-4"만 하면 간격이 나타나지 않는건가요??
-                */}
-                <div className="flex mt-6 gap-2">
-                    {feelings.map((feeling,index)=>(
-                        <button
-                        key={index}
-                        type="button"
-                        className={`p-1 rounded-lg text-sm ${selectedFeeling === feeling ? 'bg-emerald-100 text-emerald-600' : 'bg-gray-100 text-gray-500'}`}
-                        name="feeling"
-                        value={feeling}
-                        onClick={()=>handleFeelingClick(feeling)}>
-                            {feeling}
-                        </button>
-                    ))}
-                </div>
-                {/* 날씨 버튼 묶기 */}
-                <div className="flex mt-2 mb-6 gap-2">
-                    {weathers.map((weather,index)=>(
-                        <button 
-                        key={index} 
-                        type="button" 
-                        className={`p-1 rounded-lg text-sm ${selectedWeather === weather ? 'bg-blue-100 text-blue-600' : 'bg-gray-100 text-gray-500'}`}
-                        name="weather" 
-                        value={weather}
-                        onClick={()=>handleWeatherClick(weather)}>
-                            {weather}
-                        </button>
-                    ))}
-                </div>
-                
-                <textarea required name="content" placeholder="오늘 당신의 하루는 어땠나요?" className="p-1 rounded-lg resize-none border border-black-100 h-1/2 w-full"/>
-                <div>
-                    <button type="submit" className="p-2 rounded-lg bg-gray-100 text-gray-500 w-full">일기를 더 자세히 적어볼까요?</button>
-                </div>
-            </form>
+            <DiaryWriter />
             
             {/* 첫화면 오른쪽 박스 */}
             <div className="flex-col gap-4 p-4 rounded-lg bg-white border border-gray-100 h-2/3">
